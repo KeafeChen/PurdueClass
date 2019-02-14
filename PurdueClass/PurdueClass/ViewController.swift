b6ff2076496c6d5a4ae36620b6cc8a981145267c
Shunqiao <huangshunqiao@gmail.com>
2019-02-14 10:29:58 -0500

    login,signup,forgetpassword

--- PurdueClass/PurdueClass/ViewController.swift
+++ PurdueClass/PurdueClass/ViewController.swift
@@ -12,26 +12,50 @@ import Moya
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var RegisterButton: UIButton!
+    @IBOutlet weak var ForgetButton: UIButton!
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.LoginButton.layer.cornerRadius = 20
+        self.RegisterButton.layer.cornerRadius = 20
+        self.ForgetButton.layer.cornerRadius = 20
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBOutlet weak var Account: UITextField!
+    @IBOutlet weak var Username: UITextField!
     @IBOutlet weak var Password: UITextField!
     
     @IBOutlet weak var Test: UILabel!
     
+
+    
     @IBAction func LoginButton(_ sender: Any) {
-        let account:String! = Account.text
+        let username:String! = Username.text
         let password:String! = Password.text
-        if Account.text == "" || Password.text == ""{
-            Test.text = "Please Enter the account or password.\n"
+        if username == "" || password == ""{
+
+            let alert = UIAlertController(title: "Sorry", message:"You have to Enter the username and password First", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default) { _ in })
+            self.present(alert, animated: true){}
+            return
         }else{
-            let showing = "Account: \(account!), Password:\(password!)"
-            Test.text = showing
+            DoLogin(username, password)
+            //let showing = "Account: \(account!), Password:\(password!)"
+            //Test.text = showing
         }
     }
     
+    func DoLogin( _ username:String, _ psw:String){
+        //request database
+        LoginDone()
+    }
+    
+    func LoginDone(){
+        //Test.text = "Succeed!"
+    }
 }
 
