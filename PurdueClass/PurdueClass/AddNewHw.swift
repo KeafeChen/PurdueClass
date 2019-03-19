bfc3665773c6b96108ab8ef81973ab0c3eade165
sun757 <sun757@purdue.edu>
2019-03-19 00:20:40 -0400

    create "AddNewHw" viewcontroller

---
+++ PurdueClass/PurdueClass/AddNewHw.swift
@@ -0,0 +1,39 @@
+//
+//  AddNewHw.swift
+//  PurdueClass
+//
+//  Created by Sun on 3/18/19.
+//  Copyright © 2019 Qifeng Chen. All rights reserved.
+//
+
+import UIKit
+
+class AddNewHw: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backbutton(_ sender: Any) {
+    }
+    
+    @IBAction func descriptionTextField(_ sender: Any) {
+    }
+    
+    @IBAction func dueTextField(_ sender: Any) {
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PurdueClass/PurdueClass/HWdataset.swift
+++ PurdueClass/PurdueClass/HWdataset.swift
@@ -11,5 +11,7 @@ class HWdataset{
     
     
     let hwdatasets=[HWData(description: "a", date: "1"//,HWswitch: false
+        ),HWData(description: "b", date: "2"//,HWswitch: false
+        ),HWData(description: "c", date: "3"//,HWswitch: false
         )]
 }
