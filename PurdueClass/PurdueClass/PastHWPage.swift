b65222743184d6b57c27543a3f688620abedba9f
sun757 <sun757@purdue.edu>
2019-03-19 14:22:16 -0400

    Create Past due scene

--- PurdueClass/PurdueClass/PastHWPage.swift
+++ PurdueClass/PurdueClass/PastHWPage.swift
@@ -13,67 +13,67 @@ import UIKit
 
 
 
-class HWPage: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class PastHWPage: UIViewController, UITableViewDelegate, UITableViewDataSource{
     //class HeadlineTableViewCell: UITableViewCell {
     //}
-
-  //  struct HW{
-        
+    
+    //  struct HW{
+    
     //    var hwDescription: String
-      //  var dueDate: String
-   // }
+    //  var dueDate: String
+    // }
+    
     
     @IBOutlet weak var tableView: UITableView!
-    
     let data = HWdataset()
     
-   // var data = [HW]()
+    // var data = [HW]()
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate=self
         tableView.dataSource=self
         
         
-   /*    data = [
-            HW.init(hwDescription: "hello", dueDate:"world"),
-            HW.init(hwDescription: "test", dueDate:"world"),
-            HW.init(hwDescription: "hello", dueDate:"world"),
-            HW.init(hwDescription: "hello", dueDate:"world")]*/
+        /*    data = [
+         HW.init(hwDescription: "hello", dueDate:"world"),
+         HW.init(hwDescription: "test", dueDate:"world"),
+         HW.init(hwDescription: "hello", dueDate:"world"),
+         HW.init(hwDescription: "hello", dueDate:"world")]*/
         //self.tableView.register(CustomCell.self, forCellReuseIdentifier: "custom")
-      //  self.tableView.register(CustomCell.self, forCellReuseIdentifier: "hwcell")
+        //  self.tableView.register(CustomCell.self, forCellReuseIdentifier: "hwcell")
     }
-
-  //  override func didReceiveMemoryWarning() {
+    
+    //  override func didReceiveMemoryWarning() {
     //    super.didReceiveMemoryWarning()
-  //  }
+    //  }
     
     //var data: [String] = []
     //let hwDescription = ["myShell", "quiz1", "server chat" ]
     //let dueDate = ["Jan 07 11:59","Jan 20 11:59","Jan 11 11:59"]
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.hwdatasets.count
         
-
+        
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      //  let cell = self.tableView.dequeueReusableCell(withIdentifier: "hwcell")as! CustomCell
-       // cell.hwDescription = data[indexPath.row].hwDescription
+        //  let cell = self.tableView.dequeueReusableCell(withIdentifier: "hwcell")as! CustomCell
+        // cell.hwDescription = data[indexPath.row].hwDescription
         //cell.dueDate = data[indexPath.row].dueDate
-
+        
         
         
         //let text = self.hwDescription[indexPath.row]
         //cell.textLabel?.text = hwDescription[indexPath.row]
         
-        if let cell=tableView.dequeueReusableCell(withIdentifier: "hwcell", for: indexPath) as? HWcell{
+        if let cell=tableView.dequeueReusableCell(withIdentifier: "pasthwcell", for: indexPath) as? HWcell{
             cell.configureCell(hwcelldata: data.hwdatasets[indexPath.row])
             return cell
             
         }
         return UITableViewCell()
-    
+        
     }
     
     
